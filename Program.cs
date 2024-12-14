0996eb1dd34573fe52e1c59fa0793b5a28b3249a
alexelw <wattsalex2006@gmail.com>
2024-12-14 13:40:10 +0000

    seeded data

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using MyProject.Data;
+using MyProject.Data.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Get the connection string from appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+// Register the DbContext with Npgsql
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
+// Register the DbSeeder service
+builder.Services.AddTransient<IDbSeeder, DbSeederService>();
+
 var app = builder.Build();
+
+// Seed the database on application startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbSeeder = scope.ServiceProvider.GetRequiredService<IDbSeeder>();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbSeeder.Seed(dbContext); // Call the Seed method to populate the database
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
